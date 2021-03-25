ceef51f7916192eed997e3a7e8fab2473c065d18
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-03-24 18:24:45 -0600

    Add centred message into view controller

--- HelloWorldAppTests/HelloWorldViewControllerTests.swift
+++ HelloWorldAppTests/HelloWorldViewControllerTests.swift
@@ -2,11 +2,16 @@
 //  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
 //
 
+import FBSnapshotTestCase
 @testable import HelloWorldApp
 import XCTest
 
-final class HelloWorldViewControllerTests: XCTestCase {
-    func test_zero() {
-        XCTFail("Tests not yet implemented in HelloWorldViewControllerTests")
+final class HelloWorldViewControllerTests: FBSnapshotTestCase {
+    func test_view_displaysHelloWorldMessage() {
+        let bundle = Bundle(for: ViewController.self)
+        let storyboard = UIStoryboard(name: "Main", bundle: bundle)
+        let viewController = storyboard.instantiateInitialViewController() as! ViewController
+
+        FBSnapshotVerifyView(viewController.view)
     }
 }
